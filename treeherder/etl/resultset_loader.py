78c3d646f4a95e77b0fa6be7b117cad9d85c1149
Cameron Dawson <cdawson@mozilla.com>
2016-07-29 17:18:49 -0700

    fixup! Bug 1264074 - Add ability to ingest Mercurial Resultsets via Pulse

--- treeherder/etl/resultset_loader.py
+++ treeherder/etl/resultset_loader.py
@@ -181,31 +181,73 @@ class GithubPullRequestTransformer(GithubTransformer):
 
 class HgPushTransformer:
     """
-    payload:{
-        pushlog_pushes:[
-            0:{
-                time:14686073660
-                push_full_json_url:https://hg.mozilla.org/try/json-pushes?version=2&full=1&startID=134147&endID=134148
-                pushid:134148
-                push_json_url:https://hg.mozilla.org/try/json-pushes?version=2&startID=134147&endID=134148
-                user:amiyaguchi@mozilla.com
+    {
+      "root": {
+        "payload": {
+          "pushlog_pushes": [
+            {
+              "time": 14698302460,
+              "push_full_json_url": "https://hg.mozilla.org/try/json-pushes?version=2&full=1&startID=136597&endID=136598",
+              "pushid": 136598,
+              "push_json_url": " https: //hg.mozilla.org/try/json-pushes?version=2&startID=136597&endID=136598",
+              "user": " james@hoppipolla.co.uk"
             }
-        ],
-        heads:[
-            0:a6c36f356d6b6bd34778bcf20506f4424b61b63c
-        ],
-        repo_url:https://hg.mozilla.org/try
-        _meta:{
-            sent:2016-07-15T18:30:00.423330
-            routing_key:try
-            serializer:json
-            exchange:exchange/hgpushes/v1
+          ],
+          "heads": [
+            "2f77bc4f354d9ba67ea5270b2fc789f4b0521287"
+          ],
+          "repo_url": "https://hg.mozilla.org/try",
+          "_meta": {
+            "sent": "2016-07-29T22:11:18.503365",
+            "routing_key": "try",
+            "serializer": "json",
+            "exchange": "exchange/hgpushes/v1"
+          }
         }
-
+      }
+    }
     """
     def __init__(self, message_body):
         self.message_body = message_body
         self.repo_url = message_body["payload"]["repo_url"]
+
+    def transform(self, repository):
+        logger.info("transforming for {}".format(repository))
+        url = self.message_body["payload"]["pushlog_pushes"][0]["push_full_json_url"]
+        return self.fetch_resultset(url, repository)
+
+    def fetch_resultset(self, url, repository, sha=None):
+        try:
+            logger.info("fetching for {} {}".format(repository, url))
+            # there will only ever be one, with this url
+            push = fetch_json(url)["pushes"].values()[0]
+            commits = list(reversed(push["changesets"]))
+            head_commit = commits[0]
+            logger.info("got head commit: {}".format(head_commit["node"]))
+            resultset = {
+                "revision": head_commit["node"],
+                "push_timestamp": push["date"],
+                "author": head_commit["author"],
+            }
+
+            revisions = []
+            for commit in commits:
+                revisions.append({
+                    "comment": commit["desc"],
+                    "repository": repository,
+                    "author": commit["author"],
+                    "revision": commit["node"]
+                })
+
+            resultset["revisions"] = revisions
+            return resultset
+
+        except Exception as ex:
+            logger.exception("Error fetching commits", exc_info=ex)
+            newrelic.agent.record_exception(ex, params={
+                "url": url, "repository": repository, "sha": sha
+                })
+
 
 
 class PulseResultsetError(ValueError):
